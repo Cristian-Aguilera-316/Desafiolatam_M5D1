bc5f4e44a05fa0bef4da3fc6c3952bc8c1e3fc60
Cristian-Aguilera-316 <cristian.ap316@gmail.com>
2024-06-30 20:43:17 -0400

    creación del proyecto

---
+++ src/views/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home-container text-center">
+      <h1>Bienvenido a <strong>Happy Cake</strong></h1>
+      <p>El lugar de los pasteles felices</p>
+      <img src="https://img.freepik.com/vector-premium/rebanada-ilustracion-pastel-chocolate_773815-28.jpg?w=826" alt="Happy Cake Icon" />
+    </div>
+  );
+};
+
+export default Home;
